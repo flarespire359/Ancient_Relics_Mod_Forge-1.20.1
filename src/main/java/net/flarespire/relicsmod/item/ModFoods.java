6bb53c2a7ffd20e33a9520ad9def189b941879e6
flarespire359 <thomas.marment@yahoo.co.uk>
2024-06-11 21:46:55 +0100

    Food Added (3).

--- src/main/java/net/flarespire/relicsmod/item/ModFoods.java
+++ src/main/java/net/flarespire/relicsmod/item/ModFoods.java
@@ -6,8 +6,8 @@ import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
     public static final FoodProperties AETHERBERRY = new FoodProperties.Builder().nutrition(3).fast()
-            .saturationMod(0.3f).effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 200), 0.3f)
-            .effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 250), 0.3f).build();
+            .saturationMod(0.3f).effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 200), 0.75f)
+            .effect(() -> new MobEffectInstance(MobEffects.NIGHT_VISION, 250), 1f).build();
 
     public static final FoodProperties VOID_FRUIT = new FoodProperties.Builder().nutrition(3).fast()
             .saturationMod(0.3f).effect(() -> new MobEffectInstance(MobEffects.BLINDNESS, 200), 1f)
